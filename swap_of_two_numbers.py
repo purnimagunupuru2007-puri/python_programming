b57d9305c3e54863fcb00d577559b337d569c9a6
purnimagunupuru2007-puri <purnimagunupuru2007@gmail.com>
2026-07-22 18:41:24 +0530

    swap_of_two_numbers.py

---
+++ swap_of_two_numbers.py
@@ -0,0 +1,12 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+temp=a
+a=b
+b=temp
+print("after swapping")
+print("a=",a)
+print("b=",b)
+//input:5
+       10
+//output:10
+         5
