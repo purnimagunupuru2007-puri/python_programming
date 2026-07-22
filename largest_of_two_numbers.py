41b016f0cea052b40f428471a5cffab48e608267
purnimagunupuru2007-puri <purnimagunupuru2007@gmail.com>
2026-07-22 18:50:34 +0530

    largest_of_two_numbers.py

---
+++ largest_of_two_numbers.py
@@ -0,0 +1,11 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+if a>=b and a>=c:
+  print(a)
+elif b>=a and b>=c:
+  print(b)
+else:
+  print(c)
+  //input:10 25 8
+  //output:25
