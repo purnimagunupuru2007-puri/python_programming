caba4743a59fd68ce4d8927de29d4b6d7e777a57
purnimagunupuru2007-puri <purnimagunupuru2007@gmail.com>
2026-07-24 14:29:47 +0530

    palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,15 @@
+n = int(input())
+temp = n
+rev = 0
+
+while n > 0:
+    digit = n % 10
+    rev = rev * 10 + digit
+    n = n // 10
+if temp == rev:
+    print("palindrome")
+else:
+    print("not a palindrome")
+
+#input:121
+#output:palindrome
